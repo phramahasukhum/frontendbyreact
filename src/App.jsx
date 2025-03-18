70638ccc329f79b63d15a4b8d3bcca50c3ca76ce
s6519c10004 <s6519c10004@sau.ac.th>
2025-03-18 11:33:34 +0700

    frontendbyreact

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Wow from 'wowjs'
+import Wee from 'wee'
+function App() {
+  return (
+    <>
+      Welcome 
+      <Hello />
+      <h1>Hello</h1>
+      <h2>Hello</h2>
+     <h3>Hello</h3>
+
+    </>
+  )
+}
+function Hello() {
+  return (
+    <React.Fragment>
+
+    </React.Fragment>
+  ) 
+}
+
+function Hi() {
+  return (
+    <div>
+      Hi...
+    </div>
+  ) 
+}
+
+export default App
